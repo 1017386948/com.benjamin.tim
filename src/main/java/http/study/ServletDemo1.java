01f0af46f80c92c65cb0dbdcc08d9fabdee2958e
tianyr <1017386948@qq.com>
2017-10-28 00:36:58 +0800

    a

--- src/main/java/http/study/ServletDemo1.java
+++ src/main/java/http/study/ServletDemo1.java
@@ -35,10 +35,6 @@ public class ServletDemo1 extends HttpServlet {
 		this.destroy();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
